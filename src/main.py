1a3e2cc6cabc4958574a40dd9a392d63d2226b09
Mark Koh <markkohdev@gmail.com>
2017-01-21 19:34:35 -0500

    Added pagination handling for playlists.  Fetch tracks.

--- src/main.py
+++ src/main.py
@@ -3,13 +3,24 @@ import sys
 import spotipy
 import spotipy.util as sp_util
 
-scope = 'user-library-read'
+# Define the scopes that we need access to
+# https://developer.spotify.com/web-api/using-scopes/
+scope = 'user-library-read playlist-read-private'
+
+
+def print_header(message, length=50):
+    # Print a little message
+    print('\n' + ('*' * length))
+    print(message)
+    print('*' * length)
 
 
 def main():
     """
     Our main function that will get run when the program executes
     """
+    print_header('Spotify Web API Demo App')
+
     # Run our demo function
     retry = True
     while retry:
@@ -17,30 +28,54 @@ def main():
         demo()
 
         # Prompt the user to run again
-        retry_input = input('Run the program again? (Y/N): ')
+        retry_input = input('\nRun the program again? (Y/N): ')
         retry = retry_input.lower() == 'y'
 
 
 def demo():
     # Prompt the user for their username
-    username = input('What is your spotify username: ')
+    # username = input('\nWhat is your spotify username: ')
+    username = 'markster3910'
 
     # Initialize Spotipy
     token = get_token(username)
     sp = spotipy.Spotify(auth=token)
 
     # Get all the playlists for this user
-    playlists_response = sp.user_playlists(username)
-    playlists = playlists_response.get('items', [])
+    playlists = []
+    total = 1
+    # The API paginates the results, so we need to iterate
+    while len(playlists) < total:
+        playlists_response = sp.user_playlists(username, offset=len(playlists))
+        playlists.extend(playlists_response.get('items', []))
+        total = playlists_response.get('total')
+
+    # Remove any playlists that we don't own
+    playlists = [playlist for playlist in playlists if playlist.get('owner', {}).get('id') == username]
 
     # List out all of the playlists
+    print_header('Your Playlists', 30)
     for i, playlist in enumerate(playlists):
-        print('  {}.) {} - {}'.format(i + 1, playlist.get('name'), playlist.get('uri')))
+        print('  {}) {} - {}'.format(i + 1, playlist.get('name'), playlist.get('uri')))
 
     # Choose a playlist
-    playlist_choice = int(input('Choose a playlist: '))
+    playlist_choice = int(input('\nChoose a playlist: '))
     playlist = playlists[playlist_choice - 1]
     playlist_owner = playlist.get('owner', {}).get('id')
+
+    # Get the playlist tracks
+    tracks = []
+    total = 1
+    # The API paginates the results, so we need to iterate
+    while len(tracks) < total:
+        tracks_response = sp.user_playlist_tracks(playlist_owner, playlist.get('id'), offset=len(tracks))
+        tracks.extend(tracks_response.get('items', []))
+        total = tracks_response.get('total')
+
+    for i, track in enumerate(tracks):
+        track_info = track.get('track')
+        artist_names = ', '.join([artist.get('name') for artist in track_info.get('artists', [])])
+        print('  {}) {} - {}'.format(i, track_info.get('name'), artist_names))
 
 
 def get_token(username):
